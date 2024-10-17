b633e78c33db78cca591c8f629e08f89041c8ea5
nicodav28 <esteveznicolas0@gmail.com>
2024-10-17 16:50:00 -0500

    Implementa test unitairos para el microservicio de productos

--- product-service/src/test/java/com/nicodav28/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/nicodav28/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,50 @@
 package com.nicodav28.microservices.product;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody = """
+				{
+				 "name": "iPhone 15",
+				 "description": "iPhone 15 description",
+				 "price": 1500,
+				}
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/products")
+				.then()
+				.statusCode(201);
 	}
 
 }
